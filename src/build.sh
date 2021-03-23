68a302207660f6451ab77394607fe0298716842c
loloRvz <rouvinez.laurent@gmail.com>
2021-03-23 16:32:11 -0300

    -

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-/opt/wasi-sdk/bin/clang -O3 -nostdlib \
+/opt/wasi-sdk/bin/clang -O3 -nostdlib -pthread\
     -o exports.wasm exports.c \
-    -Wl,--no-entry \
+    -Wl,--shared-memory,--max-memory=131072 \
+    -Wl,--no-entry,--export=__heap_base,--export=__data_end\
     -Wl,--export=sum \
     -Wl,--export=add_one \
